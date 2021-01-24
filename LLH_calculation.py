9e881dec7aa10106cea53019a8b34aa87bf13c8b
niktoris1 <SDVrus1996>
2021-01-24 23:34:00 +0300

    Small fix with a tree tag and id

--- LLH_calculation.py
+++ LLH_calculation.py
@@ -4,7 +4,11 @@ def calculate_subtree(subtree_id):
     subtree = tree.subtree(subtree_id)
     return subtree
 
-calculate_subtree(1023).show() ##A problem here
+a = tree.get_node(1023)
+
+print(tree.get_node(1023))
+
+calculate_subtree(1023).show() ##A problem here - all good - 1023 is identifier - not tag
 
 
 
