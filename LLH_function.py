1924b76436845740e0556e38d5a83603c21c8b8d
niktoris1 <dmitriy.v.sirotkin@gmail.com>
2021-01-31 16:17:23 +0300

    Made some code, which returns a subtree with the haplotype of AB. Not sure if it works correctly, but at least it does not crash

--- LLH_function.py
+++ LLH_function.py
@@ -2,6 +2,8 @@ import math
 from build_tree import tree
 from build_tree import name_id_dict
 
+import treelib
+
 def LLH_function(iteration, coal_rate, coal_iteration, number_of_lineages, event_probability, event_type):
     return LLH_function(iteration-1) * math.exp(coal_rate * (TimeFromIteration(coal_iteration) - TimeFromIteration(coal_iteration - 1))) \
            * math.comb(DistinctLineages(TimeFromIteration(coal_iteration - 1)), 2) * EventProbability(event_type, coal_rate, DistinctLineages(TimeFromIteration(iteration)))
@@ -25,15 +27,70 @@ def EventProbability(event_type, coal_rate, distinct_lineages):
             probability = probability * coal_rate * math.comb(distinct_lineages - i + 1, 2)
         return probability
 
-class SubTree:
-    def __init__(self, tree_origin): ## subtree is defined by its origin node, old and new nucleotyde variants
-        self.origin = tree_origin
+def GetABsubtrees(A_haplotype, A_cite, B_haplotype, B_cite, covid_tree):
+    for node in covid_tree.all_nodes():
+        if node.data is not None:
+            node.data = [node.data] + [0] + [0] # here first element corresponds to node having an A_haplotype and second - to node having a B_haplotype in B_cite
 
-    def GetSubtree(self):
-        return tree.subtree(name_id_dict[self.tree_origin])
+    root_node = tree.get_node(covid_tree.root)
+    ProcessNode(root_node, A_haplotype, A_cite, B_haplotype, B_cite)
 
-    def GetLeaves(self):
-        return tree.leaves(SubTree.GetSubtree(self))
+    A_B_haplotype_list = CheckIfABHaplotype(root_node)
+
+    for node in covid_tree.all_nodes():
+        if node.data is not None:
+            node.data = node.data[0] # clean the data
+
+    return A_B_haplotype_list
+
+def ProcessNode(node, A_haplotype, A_cite, B_haplotype, B_cite):
+    if node.data is not None:
+        if node.data[0].mutation_cite == A_cite and node.data[0].new_haplotype == A_haplotype:
+            node.data[1] = 1
+        if node.data[0].mutation_cite == B_cite and node.data[0].new_haplotype == B_haplotype:
+            node.data[2] = 1
+        if node.data[0].mutation_cite == A_cite and node.data[0].new_haplotype != A_haplotype:
+            node.data[1] = 0
+        if node.data[0].mutation_cite == B_cite and node.data[0].new_haplotype != B_haplotype:
+            node.data[2] = 0
+
+    for child_node in tree.children(node.identifier):
+        ProcessNode(child_node, A_haplotype, A_cite, B_haplotype, B_cite)
+
+    return 0
+
+def CheckIfABHaplotype(node):
+    if 'ABHaplotypeNodeList' not in locals():
+        ABHaplotypeNodeList = []
+
+    if node.data is not None:
+        if node.data[1] == 1 and node.data[2] == 1:
+            ABHaplotypeNodeList = ABHaplotypeNodeList + node
+        else:
+            for child in tree.children(node.identifier):
+                CheckIfABHaplotype(child)
+
+    if 'ABHaplotypeNodeList' not in locals():
+        return []
+    else:
+        return ABHaplotypeNodeList
+
+
+
+class TestHaplotypeAB:
+    def __init__(self, A_haplotype, A_cite, B_haplotype, B_cite, phylogenetic_tree):
+        self.A_haplotype = A_haplotype
+        self.A_cite = A_cite
+        self.B_haplotype = B_haplotype
+        self.B_cite = B_cite
+        self.phylogenetic_tree = phylogenetic_tree
+
+    def getLikelyhoodAB(self):
+        return 0
+
+a = GetABsubtrees('G', 100, 'G', 200, tree)
+
+print(a)
 
 
 
--- build_tree.py
+++ build_tree.py
@@ -1,10 +1,16 @@
 from treelib import Node, Tree
 from initialize_tree import  raw_nodes
 from parse_mutations import mutations
+import re
+
+
+def getcite(mutation_name): # gets a cite name from the name of mutation
+    return re.findall('\d+', mutation_name)[0]
 
 class MutationOnNode: # defines mutztion on specific node
     def __init__(self, mutation_name, old_nucleotyde, new_nucleotyde):
         self.mutation_name = mutation_name
+        self.mutation_cite = getcite(mutation_name)
         self.old_nucleotyde = old_nucleotyde
         self.new_nucleotyde = new_nucleotyde
 
@@ -12,7 +18,10 @@ class MutationOnNode: # defines mutztion on specific node
 name_id_dict = {} # dictionary in type id: name
 
 def add_children(tree, node):
-    tree.create_node(node["name"], node["id"], parent=node["parentid"])
+    if node["parentid"] is not None:
+        tree.create_node(node["name"], node["id"], parent=node["parentid"])
+    else:
+        tree.create_node(node["name"], node["id"])
     name_id_dict[node["name"]] = node["id"]
     for child_node in node["children"]:
         add_children(tree, child_node)
@@ -29,17 +38,17 @@ for children_node in raw_nodes["children"]:
 #tree.save2file('tree.txt')
 
 for mutation in mutations: # here for every node we write a mutation in it
-    if len(mutation.mutation_nodes) > 5:
-        print("Mutation ", mutation.mutation_name, " has ", len(mutation.mutation_nodes), " mutations")
-        for mutation_node in mutation.mutation_nodes:
-            print(mutation_node.old_nucleotyde, " maps to ", mutation_node.new_nucleotyde)
+    #if len(mutation.mutation_nodes) > 5:
+    #    print("Mutation ", mutation.mutation_name, " has ", len(mutation.mutation_nodes), " mutations")
+    #    for mutation_node in mutation.mutation_nodes:
+    #        print(mutation_node.old_nucleotyde, " maps to ", mutation_node.new_nucleotyde)
     for mutation_node in mutation.mutation_nodes: # we take all possible couples of old-new nycleotydes
         for node_name in mutation_node.nodes_names:
             if node_name[-3:] == '[F]' or node_name[-3:] == '[B]':
                 node_name = node_name[:-3]
             chosen_node = tree.get_node(name_id_dict[node_name])
             chosen_node.data = MutationOnNode(mutation.mutation_name, mutation_node.old_nucleotyde, mutation_node.new_nucleotyde)
-            # Костыль. Мы оставляем на ноде в дереве только самые важные данные - место мутации, а также стратовый и новый нуклеотид
+            # Костыль. Мы оставляем на ноде в дереве только самые важные данные - название мутации, место мутции, мутации, а также стратовый и новый нуклеотид
 
 
 
