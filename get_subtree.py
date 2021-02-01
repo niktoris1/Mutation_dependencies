8dc304e15103e6acca98d223695f970f68f96b32
niktoris1 <dmitriy.v.sirotkin@gmail.com>
2021-02-01 18:15:48 +0300

    Now we store tree as a vertices with initial mutations. Still, there is a error which needs to be fixed and a function SubtreesFromRoots to be written

--- get_subtree.py
+++ get_subtree.py
@@ -4,9 +4,16 @@ from build_tree import name_id_dict
 
 import treelib
 
+from treelib import tree
+
 class SubtreeCreation:
 
-    def ProcessNode(self, root_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, current_A_state, current_B_state):
+
+    AB_haplotype_subtree_roots = []
+    not_AB_haplotype_subtree_roots = []
+
+
+    def ProcessNode(self, root_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, current_A_state, current_B_state, base_tree):
         if root_node.data is not None:
             if root_node.data[0].mutation_cite == A_cite and root_node.data[0].new_nucleotyde == A_nucleotyde:
                 root_node.data[1] = 1
@@ -22,22 +29,48 @@ class SubtreeCreation:
                 current_B_state = 0
 
 
-        for child_node in covid_tree.children(root_node.identifier):
-            SubtreeCreation.ProcessNode(self, child_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, current_A_state, current_B_state)
+        for child_node in base_tree.children(root_node.identifier):
+            SubtreeCreation.ProcessNode(self, child_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, current_A_state, current_B_state, base_tree)
 
-    def CheckIfABHaplotype(self, node):
-
-        if node.data is not None:
+    def ABHaplotypeIsPresent(self, node): #checks if both haplotypes are pressent
+        if node.data is None:
+            return 0
+        else:
             if node.data[1] == 1 and node.data[2] == 1:
-                SubtreeCreation.AB_haplotype_node_list.append(node)
-        for child in covid_tree.children(node.identifier):
-            SubtreeCreation.CheckIfABHaplotype(self, child)
+                return 1
+            else:
+                return 0
 
-        return SubtreeCreation.AB_haplotype_node_list
+    def GetParentNode(self, node, base_tree):
+        return base_tree.get_node(node.predecessor)
 
-    AB_haplotype_node_list = [] # a local variable for the class
+    def CheckIfABHaplotype(self, node, base_tree):
+
+        if node.is_root():
+            if SubtreeCreation.ABHaplotypeIsPresent(self, node) == 1:
+                SubtreeCreation.AB_haplotype_subtree_roots.append(node)
+            else:
+                SubtreeCreation.not_AB_haplotype_subtree_roots.append(node)
+
+        else:
+            if SubtreeCreation.ABHaplotypeIsPresent(self, node) and not SubtreeCreation.ABHaplotypeIsPresent(self, SubtreeCreation.GetParentNode(self, node, base_tree)):
+                SubtreeCreation.AB_haplotype_subtree_roots.append(node)
+            if not SubtreeCreation.ABHaplotypeIsPresent(self, node) and SubtreeCreation.ABHaplotypeIsPresent(self, SubtreeCreation.GetParentNode(self, node, base_tree)):
+                SubtreeCreation.not_AB_haplotype_subtree_roots.append(node)
+
+
+
+        for child in base_tree.children(node.identifier):
+            SubtreeCreation.CheckIfABHaplotype(self, child, base_tree)
+
+        return [SubtreeCreation.AB_haplotype_subtree_roots, SubtreeCreation.not_AB_haplotype_subtree_roots] # we return two arrays - the roots of defining subtrees
+
+
 
     def GetABsubtrees(self, A_nucleotyde, A_cite, B_nucleotyde, B_cite, base_tree): # here we do not look at what the old haplotype is
+
+        # also, this part of code can be done better by simply getting said mutations from data. Still, no harm done.
+
         for node in base_tree.all_nodes():
             if node.data is not None:
                 node.data = [node.data] + [0] + [0] # here first element corresponds to node having an A_nucleotyde and second - to node having a B_nucleotyde in B_cite
@@ -59,15 +92,18 @@ class SubtreeCreation:
             else:
                 starting_B_state = 1
 
-        SubtreeCreation.ProcessNode(self, root_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, starting_A_state, starting_B_state) # at the root there are no mutations
+        SubtreeCreation.ProcessNode(self, root_node, A_nucleotyde, A_cite, B_nucleotyde, B_cite, starting_A_state, starting_B_state, base_tree) # at the root there are no mutations
 
-        list_of_AB_roots = SubtreeCreation.CheckIfABHaplotype(self, root_node)
+        list_of_AB_roots = SubtreeCreation.CheckIfABHaplotype(self, root_node, base_tree)
 
         for node in base_tree.all_nodes():
             if node.data is not None:
                 node.data = node.data[0] # clean the data
 
         return list_of_AB_roots
+
+    def SubtreesFromRoots(self):
+        return 0
 
 
 
