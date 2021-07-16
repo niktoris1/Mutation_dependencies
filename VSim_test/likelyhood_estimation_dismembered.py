38fd5562abeb1eca1c747d8d772a59be2488fdc7
niktoris1 <dmitriy.v.sirotkin@gmail.com>
2021-07-16 19:33:57 +0300

    Created preprocessing, fixed dict to list transition

--- VSim_test/likelyhood_estimation_dismembered.py
+++ VSim_test/likelyhood_estimation_dismembered.py
@@ -16,7 +16,8 @@ tdm = simulation.gettdm() #get tdm object
 trees_funct, trees_neutral = tdm.Dismember() #перед получением таблиц, нужно разчленить дерево
 #получение таблиц
 event_table_funct, event_table_neutral = tdm.getEventTable() #[{time: [n_samples, n_coals]}]
-sample_fraction_table = tdm.getSampleFracTable([0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4, 2.7])
+brackets = [0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4, 2.7]
+sample_fraction_table = tdm.getSampleFracTable(brackets)
 
 
 class LikelyhoodEstimationDismembered:
@@ -31,77 +32,111 @@ class LikelyhoodEstimationDismembered:
     def __init__(self, event_table_funct=None, event_table_neutral=None, sample_fraction_table=None): # here we have not tree, but tables
 
         # for now we do not care if the tree contains mutation or not
-        event_table_funct[0].update(event_table_neutral[0]) # merges dictionaries in-place
-        event_table = list(event_table_funct[0].items())
+        event_table = event_table_funct[0] + event_table_neutral[0] # merges lists
 
         #format of data is [left timestamp, number of samples, number_of coals, fraction]
         self.bracket_data = []
         for bracket in sample_fraction_table:
-            self.bracket_data.append([[], [], [], [bracket, sample_fraction_table[bracket]] ])
+            self.bracket_data.append([[], [], [], bracket[0], bracket[1]])
 
-        for i in range(len(self.bracket_data)):
-            if i < len(self.bracket_data) - 1:
-                time_start = self.bracket_data[i][3][0]
-                time_finish = self.bracket_data[i+1][3][0]
-            else:
-                time_start = self.bracket_data[i][3][0]
-                time_finish = 999
-            for event in event_table:
-                if event[0] >= time_start and event[0] < time_finish:
-                    self.bracket_data[i][0].append(event[0])
-                    self.bracket_data[i][1].append(event[1][0])
-                    self.bracket_data[i][2].append(event[1][1])
 
-        # bracket_data is a list of brackets Each correspons to a timeframe
+        self.timestamps = [self.bracket_data[i][4] for i in range(len(self.bracket_data))]
+
+        # bracket_data is a list of brackets Each corresponds to a timeframe
         # each bracket is a list of 4 values
         # 1) list of times
         # 2) list of indicators if the event is sample
         # 3) list of indicators if the event is coalescence
+        # 4) bracket timstamp
+        # 5) bracket fraction
 
 
-
-        self.event_array = []
         for i in range(len(self.bracket_data)):
-            data_piece = self.bracket_data[i]
-            for j in range(len(data_piece[0])):
-                event = self.Event(time = data_piece[0][j], is_sample=data_piece[1][j],
-                    is_coal = data_piece[2][j], timestamp=data_piece[3][0],
-                    fraction=data_piece[3][1])
+            if i < len(self.bracket_data) - 1:
+                time_start = self.bracket_data[i][3]
+                time_finish = self.bracket_data[i+1][3]
+            else:
+                time_start = self.bracket_data[i][3]
+                time_finish = 999
+            for event in event_table:
+                if event[0] >= time_start and event[0] < time_finish:
+                    self.bracket_data[i][0].append(event[0])
+                    self.bracket_data[i][1].append(event[1])
+                    self.bracket_data[i][2].append(event[2])
 
-                self.event_array.append(event)
+
+        self.event_array = [[] for _ in range(len(self.bracket_data))]
+        for bracket_num in range(len(self.bracket_data)):
+            current_bracket = self.bracket_data[bracket_num]
+            for j in range(len(current_bracket[0])):
+                event = self.Event(time = current_bracket[0][j], is_sample=current_bracket[1][j],
+                    is_coal = current_bracket[2][j], timestamp=current_bracket[3],
+                    fraction=current_bracket[4])
+
+                self.event_array[bracket_num].append(event)
+
+            def takeTime(elem):
+                return elem.time
+
+            self.event_array[bracket_num].sort(key=takeTime)
 
         self.number_of_events = len(self.event_array)
 
-    def LLH_function(self, coal_rate, needed_timestamp): # this must be rewritten from the tables perspective
+    def LLH_function(self, coal_rate):
+
+        # returns LLH function for all time brackets
 
         # Event(time, is_sample, is_coal, timestamp, fraction)
 
-        events = []
+
+
+        distinct_lineages_array = [[] for _ in range(len(self.event_array)) ]
+        event_probability_array = [[] for _ in range(len(self.event_array)) ]
+
+        for i in range(len(self.event_array)):
+            for j in range(len(self.event_array[i])):
+                distinct_lineages_array[i].append(0)
+                event_probability_array[i].append(0)
+
+
+
+        for i in range(len(self.event_array)):
+            for j in range(len(self.event_array[i])):
+                if i == 0 and j == 0:
+                    distinct_lineages_array[i][j] = 0
+                elif i > 0 and j == 0:
+                    distinct_lineages_array[i][j] = distinct_lineages_array[i - 1][len(distinct_lineages_array[i - 1]) - 1]
+                elif j > 0:
+                    distinct_lineages_array[i][j] = distinct_lineages_array[i][j - 1]
+
+                if self.event_array[i][j].is_coal == 1:
+                    distinct_lineages_array[i][j] = distinct_lineages_array[i][j] + 1
+                if self.event_array[i][j].is_sample == 1:
+                    distinct_lineages_array[i][j] = distinct_lineages_array[i][j] - 1
+
+        for i in range(len(self.event_array)):
+            for j in range(len(self.event_array[i])):
+                if self.event_array[i][j].is_sample == 1:
+                    event_probability_array[i][j] = 1
+                else:
+                    probability = 1
+                    for iter in range(0, 1): # TODO - update for a bigger number of children
+                        probability = probability * coal_rate * math.comb(distinct_lineages_array[i][j] - iter + 1, 2)
+                    event_probability_array[i][j] = probability
+
+
         for i in range(self.number_of_events):
-            if self.event_array[i].timestamp == needed_timestamp:
-                events.append(self.event_array[i])
-
-        def takeTime(elem):
-            return elem.time
-
-        events.sort(key=takeTime)
-
-        LLH_values = [0] * len(events)
-        distinct_lineages_array = [0] * len(events)
-        event_probability_array = [0] * len(events)
-        addition = [0] * len(events)
-
-        for i in range(len(events)): # TODO - check if colescent event is correct and sampling event is correct
-            if i > 0:
-                distinct_lineages_array[i] = distinct_lineages_array[i - 1]
-            if events[i].is_coal == 1:
-                distinct_lineages_array[i] = distinct_lineages_array[i] + 1
-
-        for i in range(len(events)):
             event_probability_array[i] = LikelyhoodEstimationDismembered.EventProbability(self, coal_rate, i, distinct_lineages_array)
 
-        for i in range(1, len(events)):
-            addition[i] = (- coal_rate * (events[i].time - events[i-1].time) *
+        for timestamp_number in range(len(self.timestamps)):
+            LLH_value_by_timestamp = [[0] for _ in range(len(self.timestamps))]
+
+
+        LLH_values = [[0] for _ in range(self.number_of_events)]
+        addition = [[0] for _ in range(self.number_of_events)]
+
+        for i in range(1, self.number_of_events):
+            addition[i] = (- coal_rate * (self.event_array[i].time - self.event_array[i-1].time) *
                math.comb(distinct_lineages_array[i], 2)) + \
                           math.log(event_probability_array[i])
 
@@ -109,18 +144,14 @@ class LikelyhoodEstimationDismembered:
 
         return LLH_values[-1]
 
-    def DistinctLineages(self, time):
-        iteration = IterationFromTime(time, es=self.es)
-        return self.distinct_lineages[iteration]
-
-    def EventProbability(self, coal_rate, iteration, distinct_lineages):
+    def EventProbability(self, coal_rate, iteration, distinct_lineages_array):
 
         if self.event_array[iteration].is_coal == 0:
             return 1
         else:
             probability = 1
             for i in range(0, 1): # TODO - update for a bigger number of children
-                probability = probability * coal_rate * math.comb(distinct_lineages[iteration] - i + 1, 2)
+                probability = probability * coal_rate * math.comb(distinct_lineages_array[iteration] - i + 1, 2)
             return probability
 
 
@@ -128,7 +159,7 @@ class LikelyhoodEstimationDismembered:
 
         print("GETTING ESTIMATION")
         #start_coal_rate=20 # will have to estimate
-        wrapper = lambda coal_rate: - self.LLH_function(coal_rate=coal_rate, needed_timestamp=needed_timestamp)
+        wrapper = lambda coal_rate: - self.LLH_function(coal_rate=coal_rate)
 
         LLH_optimised = optimize.minimize_scalar(fun=wrapper, bounds = (0.0001, 1000), bracket = (0.0001, 10), method='Bounded', tol=1e-5)
         #LLH_optimised = optimize.minimize(fun=wrapper, x0=20, method='Nelder-Mead', tol=1e-2)
